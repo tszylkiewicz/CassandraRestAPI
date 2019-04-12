c19ec0f6eb189891674f52ba44ed123aab2063ce
tszylkiewicz <43911837+tszylkiewicz@users.noreply.github.com>
2019-04-12 21:46:02 +0200

    [ADD] Creating data model

---
+++ src/main/java/model/GPS.java
@@ -0,0 +1,36 @@
+package model;
+
+import org.springframework.data.cassandra.mapping.Table;
+
+@Table("gps")
+public class GPS {
+    private float lat;
+    private float lng;
+
+    public GPS() {
+        super();
+    }
+
+    public GPS(float lat, float lng) {
+        super();
+        this.lat = lat;
+        this.lng = lng;
+
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLng() {
+        return lng;
+    }
+
+    public void setLng(float lng) {
+        this.lng = lng;
+    }
+}
